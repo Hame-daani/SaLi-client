530f2a501014e41359891c9249bc09ffa9f12cda
Sadegh Hamedani <s.hamedanipr@gmail.com>
2020-02-27 14:26:35 +0330

    fix doc strings

--- AI.py
+++ AI.py
@@ -12,25 +12,31 @@ class AI:
         self.rows = 0
         self.cols = 0
         self.path_for_my_units: List[Path] = None
-    # this function is called in the beginning for deck picking and pre process
 
     def pick(self, world: World):
+        """
+        this function is called in the beginning for deck picking and pre process
+        """
         start_time = time.time()
         Logs.show_log("pick started!")
         PickHandler.pick(self, world)
         Logs.show_log(f"pick finished! time:{time.time()-start_time}")
-    # it is called every turn for doing process during the game
 
     def turn(self, world: World):
+        """
+        it is called every turn for doing process during the game
+        """
         start_time = time.time()
         Logs.show_log(f"turn {world.get_current_turn()} started.")
         TurnHandler.turn(self, world)
         Logs.show_log(
             f"turn {world.get_current_turn()} finished. time: {time.time()-start_time}")
-    # it is called after the game ended and it does not affect the game.
-    # using this function you can access the result of the game.
-    # scores is a map from int to int which the key is player_id and value is player_score
 
     def end(self, world: World, scores):
+        """
+        it is called after the game ended and it does not affect the game.
+        using this function you can access the result of the game.
+        scores is a map from int to int which the key is player_id and value is player_score
+        """
         print("end started!")
         print("My score:", scores[world.get_me().player_id])
