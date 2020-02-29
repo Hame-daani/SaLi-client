d13914b4dd66708d867680ce50d51892853e5cf4
Sadegh Hamedani <s.hamedanipr@gmail.com>
2020-02-29 14:23:45 +0330

    format code!

--- core/TurnHandler.py
+++ core/TurnHandler.py
@@ -1,8 +1,8 @@
 import random
 from typing import List
 
-from core.PickHandler import *
-from model import *
+from core.PickHandler import PickHandler
+from model import Cell, Logs, Path, Spell, SpellTarget, SpellType, Unit
 from world import World
 
 
@@ -24,48 +24,55 @@ class TurnHandler:
         # Process Upgrades
         self.upgrade_process(world)
 
-    #enemy = false familiar= true
-    #این نابع چک میکند که یونیت داده شده جز آن دسته خواسته شده است یا نه
-    def Check_Enemy_or_familiar_Unit(self,world:World,unit:Unit,enemy_familiar:bool)->bool:
-        #-----------------------------
+    # enemy = false familiar= true
+    # این نابع چک میکند که یونیت داده شده جز آن دسته خواسته شده است یا نه
+    def Check_Enemy_or_familiar_Unit(self, world: World, unit: Unit, enemy_familiar: bool) -> bool:
+        # -----------------------------
         self.My_Player = world.get_me()
         self.My_First_Enemy = world.get_first_enemy()
         self.My_Second_Enemy = world.get_second_enemy()
         self.My_Freind = world.get_friend()
-        #------------------------------
-        All_units_own=self.My_Player.units
-        All_units_enemy=self.My_First_Enemy.units
+        # ------------------------------
+        All_units_own = self.My_Player.units
+        All_units_enemy = self.My_First_Enemy.units
 
         for unit in self.My_Freind.units:
             All_units_own.append(unit)
         for unit in self.My_Second_Enemy.units:
             All_units_enemy.append(unit)
-        #--------------------------------
-        if(enemy_familiar==False):
-            if(All_units_enemy.__contains__(unit)):return True
-            else:return False
+        # --------------------------------
+        if(enemy_familiar == False):
+            if(All_units_enemy.__contains__(unit)):
+                return True
+            else:
+                return False
         else:
-            if(All_units_own.__contains__(unit)):return True
-            else:return False
-    #این تابع فاصله منهتنی یک سلول نسبت به یک سلول دیگر را برمیگردانذ
-    def Manhatan_Distance(self,BeginingCell:Cell,EndingCell:Cell)->int:
+            if(All_units_own.__contains__(unit)):
+                return True
+            else:
+                return False
+    # این تابع فاصله منهتنی یک سلول نسبت به یک سلول دیگر را برمیگردانذ
+
+    def Manhatan_Distance(self, BeginingCell: Cell, EndingCell: Cell) -> int:
         import math
-        dist=math.fabs(BeginingCell.row-EndingCell.row)+math.fabs(BeginingCell.col-EndingCell.col)
+        dist = math.fabs(BeginingCell.row-EndingCell.row) + \
+            math.fabs(BeginingCell.col-EndingCell.col)
         return dist
-    #این تابع باید چک کند در فاصله k از یک خانه یونیتی وجود دارد
-    def Check_Unit_Number_in_Manhatan(self,world:World,Center:Cell,distance:int,enemy_familiar:bool)->int:
-        All_path_in_map=world.get_map().paths
-        All_path_have_Center_Cell=[]
-        Number_unit=0
+    # این تابع باید چک کند در فاصله k از یک خانه یونیتی وجود دارد
+
+    def Check_Unit_Number_in_Manhatan(self, world: World, Center: Cell, distance: int, enemy_familiar: bool) -> int:
+        All_path_in_map = world.get_map().paths
+        All_path_have_Center_Cell = []
+        Number_unit = 0
         for path in All_path_in_map:
             if(path.cells.__contains__(Center)):
                 All_path_have_Center_Cell.append(path)
         for path in All_path_have_Center_Cell:
             for cell in path.cells:
-                if(self.Manhatan_Distance(Center,cell)<=distance):
+                if(self.Manhatan_Distance(Center, cell) <= distance):
                     for unit in world.get_cell_units(cell=cell):
-                        if(self.Check_Enemy_or_familiar_Unit(world,unit,enemy_familiar)==True):
-                            Number_unit=Number_unit+1
+                        if(self.Check_Enemy_or_familiar_Unit(world, unit, enemy_familiar) == True):
+                            Number_unit = Number_unit+1
         return Number_unit
 
     # استراتیزی...
@@ -110,9 +117,9 @@ class TurnHandler:
         path_to_enemy.extend(
             [path for path in paths_from_me if path in paths_to_second_enemy])
         path_for_my_units = min(path_to_enemy, key=lambda p: len(p.cells))
-        #----------------------------------
-        self.path_for_my_units_=path_for_my_units
-        #----------------------------------
+        # ----------------------------------
+        self.path_for_my_units_ = path_for_my_units
+        # ----------------------------------
         return path_for_my_units
 
     def put_units(self, world: World, path_for_my_units: Path):
@@ -130,78 +137,85 @@ class TurnHandler:
 # اسپل محیطی و اسپل یونیتی
 # اسپل های محیطی با توجه ب نوعشون باید در بهترین مکان زده بشن مثلا اسپل محیطی سم ک برای دشمن زده میشه باید در مکانی زده بشه ک بیشترین دشمن در اونجا قرار داره
 # و بقیه اسپل ها هم همینطور
-    def GradeCell(self,units:List["Unit"]) ->int:
-        grad=0
+    def GradeCell(self, units: List["Unit"]) -> int:
+        grad = 0
         for unit in units:
-            if(unit.hp<5):
-                grad=grad+4
-            elif(unit.hp<10):
-                grad=grad+2
+            if(unit.hp < 5):
+                grad = grad+4
+            elif(unit.hp < 10):
+                grad = grad+2
             else:
-                grad=grad+1
+                grad = grad+1
         return grad
-    def BestCell(self,world:World, received_spell:Spell):
-        #-----------------------------
+
+    def BestCell(self, world: World, received_spell: Spell):
+        # -----------------------------
         self.My_Player = world.get_me()
         self.My_First_Enemy = world.get_first_enemy()
         self.My_Second_Enemy = world.get_second_enemy()
         self.My_Freind = world.get_friend()
-        #------------------------------
+        # ------------------------------
 
-        All_units_own=self.My_Player.units
-        All_units_enemy=self.My_First_Enemy.units
+        All_units_own = self.My_Player.units
+        All_units_enemy = self.My_First_Enemy.units
 
         for unit in self.My_Freind.units:
             All_units_own.append(unit)
         for unit in self.My_Second_Enemy.units:
             All_units_enemy.append(unit)
-        #--------------------------------
+        # --------------------------------
         Select_Cell = None
 
-        if(received_spell.target==SpellTarget.ENEMY):
-            num_enemy_around_cell=0
-            grade_cell=0
-            #best cell for HP(Posion And Damage)
+        if(received_spell.target == SpellTarget.ENEMY):
+            num_enemy_around_cell = 0
+            grade_cell = 0
+            # best cell for HP(Posion And Damage)
             for unit in All_units_enemy:
 
-                target = world.get_area_spell_targets(center=unit.cell,spell=received_spell)
-                grade_cell_temp=self.GradeCell(target)
+                target = world.get_area_spell_targets(
+                    center=unit.cell, spell=received_spell)
+                grade_cell_temp = self.GradeCell(target)
 
-                print("target : ",target.__len__(),"   grade : ",grade_cell_temp)
+                print("target : ", target.__len__(),
+                      "   grade : ", grade_cell_temp)
 
-                if(target.__len__()>=num_enemy_around_cell & grade_cell_temp>=grade_cell):
-                    print("garde : ",grade_cell_temp)
-                    num_enemy_around_cell=target.__len__()
-                    grade_cell=grade_cell_temp
-                    Select_Cell=unit.cell
+                if(target.__len__() >= num_enemy_around_cell & grade_cell_temp >= grade_cell):
+                    print("garde : ", grade_cell_temp)
+                    num_enemy_around_cell = target.__len__()
+                    grade_cell = grade_cell_temp
+                    Select_Cell = unit.cell
 
-            print("target enemy : ",Select_Cell)
+            print("target enemy : ", Select_Cell)
 
-        elif(received_spell.target==SpellTarget.ALLIED):
+        elif(received_spell.target == SpellTarget.ALLIED):
 
-            if(received_spell.type==SpellType.HP):
+            if(received_spell.type == SpellType.HP):
                 num_enemy_around_cell = 0
                 grade_cell = 0
                 for unit in All_units_own:
-                    target = world.get_area_spell_targets(center=unit.cell, spell=received_spell)
+                    target = world.get_area_spell_targets(
+                        center=unit.cell, spell=received_spell)
                     grade_cell_temp = self.GradeCell(target)
-                    print("target : ", target.__len__(), "   grade : ", grade_cell_temp)
+                    print("target : ", target.__len__(),
+                          "   grade : ", grade_cell_temp)
                     if (target.__len__() >= num_enemy_around_cell & grade_cell_temp >= grade_cell):
-                        print("grade : ",grade_cell_temp)
+                        print("grade : ", grade_cell_temp)
                         num_enemy_around_cell = target.__len__()
                         grade_cell = grade_cell_temp
                         Select_Cell = unit.cell
             else:
-                #cell for Haste And Duolicate
+                # cell for Haste And Duolicate
                 num_enemy_around_cell = 0
                 for unit in All_units_own:
-                    target = world.get_area_spell_targets(center=unit.cell, spell=received_spell)
+                    target = world.get_area_spell_targets(
+                        center=unit.cell, spell=received_spell)
                     if (target.__len__() > num_enemy_around_cell):
                         num_enemy_around_cell = target.__len__()
                         Select_Cell = unit.cell
 
-            print("target alied :", Select_Cell,"  nume unit : ",world.get_cell_units(Select_Cell).__len__())
-        print("Best Select_Cell : ",Select_Cell)
+            print("target alied :", Select_Cell, "  nume unit : ",
+                  world.get_cell_units(Select_Cell).__len__())
+        print("Best Select_Cell : ", Select_Cell)
         return Select_Cell
 
     def spell_process(self, world: World):
@@ -219,9 +233,8 @@ class TurnHandler:
     def put_area_spell(self, received_spell: Spell, world: World):
         """
         """
-        Target_Cell=self.BestCell(world,received_spell)
-        world.cast_area_spell(center=Target_Cell,spell=received_spell)
-
+        Target_Cell = self.BestCell(world, received_spell)
+        world.cast_area_spell(center=Target_Cell, spell=received_spell)
 
     def put_unit_spell(self, world: World, received_spell: Spell):
         """
