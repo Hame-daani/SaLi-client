0f0be18d30a83ae020d0a0f6a4cdbf40d8bc8fa4
Prince <prince137667@gmail.com>
2020-02-29 15:57:58 +0330

    add picture

--- core/PickHandler.py
+++ core/PickHandler.py
@@ -3,7 +3,7 @@ from typing import List
 
 from model import Path
 from world import World
-
+from model import *
 
 class PickHandler:
     """
@@ -17,6 +17,12 @@ class PickHandler:
         map = world.get_map()
         self.rows = map.row_num
         self.cols = map.col_num
+        #-----------------------------------
+        self.My_Player = world.get_me()
+        self.My_First_Enemy = world.get_first_enemy()
+        self.My_Second_Enemy = world.get_second_enemy()
+        self.My_Freind = world.get_friend()
+        #------------------------------------
         # choosing all flying units
         all_base_units = world.get_all_base_units()
         my_hand = [
